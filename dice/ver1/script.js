edff572589e65e6585edc086cc50ed44f9aad89d
SAHOON KIM <sahuni@gmail.com>
2024-12-26 13:44:02 +0900

    Update script.js

--- dice/ver1/script.js
+++ dice/ver1/script.js
@@ -4,7 +4,7 @@ document.getElementById("dice-area").addEventListener("click", () => {
   
     const interval = setInterval(() => {
       const randomDice = Math.floor(Math.random() * 6) + 1;
-      diceImage.src = `img/dice-${randomDice}-rmbg.png`; // 주사위 이미지 변경
+      diceImage.src = `../img/dice-${randomDice}-rmbg.png`; // 주사위 이미지 변경
       rollCount++;
       if (rollCount > 5) { // 0.5초 동안 100ms 간격으로 반복
         clearInterval(interval);
